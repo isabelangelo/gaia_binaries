8f4738c08bdd8c6d1695ddcede6cece1553ae4d7
Isabel Angelo <isabelangelo@astro.ucla.edu>
2023-04-17 15:48:53 -0400

    asserted that binary GALAH/Gaia xmatch is one-to-one, removed duplicates based on Gaia ID

--- process_GALAH_data/GALAH_catalog_gaia_xmatch.py
+++ process_GALAH_data/GALAH_catalog_gaia_xmatch.py
@@ -15,7 +15,7 @@ catalogs = Vizier.find_catalogs('Traven')
 catalogs = {k: v for k, v in catalogs.items() if 'J/A+A/638/A145' in k}
 Vizier.ROW_LIMIT = -1
 catalogs = Vizier.get_catalogs(catalogs.keys())
-GALAH_binary_catalog = catalogs[0].to_pandas()
+GALAH_binary_labels = catalogs[0].to_pandas()
 
 # reformat Gaia designation column
 GALAH_xmatch['designation'] = [i.decode().replace('EDR3', 'DR3') for i in GALAH_xmatch['designation']]
@@ -50,31 +50,35 @@ GALAH_stars.to_csv(GALAH_stars_filename, index=False)
 print('GALAH stars + Gaia designations saved to {}'.format(GALAH_stars_filename))
 
 
-########### NOTE: this needs to be re-written, but I'm trying to focus on the single stars at the moment ########
 ########### save GALAH binary catalog designations ###################
-GALAH_binaries = pd.merge(GALAH_binary_catalog, GALAH_xmatch, left_on='spectID', right_on='sobject_id')
-binary_columns_to_save = ['spectID', 'designation', 'dr3_source_id', 'Teff1-50',
-                  'logg1-50', '__Fe_H_-50', 'vbroad1-50','Teff2-50', 'logg2-50', 
-                   'vbroad2-50', 'RV1-50', 'RV2-50']
-GALAH_binaries_to_save = GALAH_binaries[binary_columns_to_save].rename(
-	columns={
-	'spectID':'galah_sobject_id',
-    "designation": "gaia_designation",
-	'dr3_source_id':'dr3_source_id',
-	'Teff1-50':'galah_teff1',
-	'logg1-50':'galah_logg1',
-    '__Fe_H_-50':'galah_feh',
-	'vbroad1-50':'galah_vbroad1',
-	'Teff2-50':'galah_teff2',
-	'logg2-50':'galah_logg2',
-	'vbroad2-50':'galah_vbroad2',
-	'RV1-50':'galah_rv1',
-	'RV2-50':'galah_rv2'
-	})
+GALAH_binaries = pd.merge(GALAH_xmatch[GALAH_xmatch_cols], GALAH_binary_labels, 
+                left_on='sobject_id', right_on = 'spectID', validate='one_to_one')
 
+# remove DR3 designations that map to multiple GALAH sobject_id
+GALAH_binaries = GALAH_binaries.drop_duplicates(subset=['designation'], keep=False)
+GALAH_binaries_to_save = GALAH_binaries.rename(
+    columns={
+    'spectID':'sobject_id',
+    'Teff1-50':'galah_teff1',
+    'logg1-50':'galah_logg1',
+    '__Fe_H_-50':'galah_feh',
+    'vbroad1-50':'galah_vbroad1',
+    'Teff2-50':'galah_teff2',
+    'logg2-50':'galah_logg2',
+    'vbroad2-50':'galah_vbroad2',
+    'RV1-50':'galah_rv1',
+    'RV2-50':'galah_rv2'
+    })
 GALAH_binaries_filename = './GALAH_data_tables/GALAH_binary_catalog.csv'
 GALAH_binaries_to_save.to_csv(GALAH_binaries_filename, index=False)
 print('GALAH binaries + Gaia designations saved to {}'.format(GALAH_binaries_filename))
+
+# add tests that verify that the binary xmatch is working
+# number of unique sobject_id and designation should match in the final GALAH_binaries
+# number of objects in final GALAH_binaries should match rows with count=1 before duplicates dropped
+# len(final GALAH_binaries) + objects wtih count>1 before = len(GALAH_binaries before duplicates dropped)
+# see cell in jupyter notebook scratch_workspace to see how to compute the counts
+
 
 
 
--- train_cannon_model/plot_galah_versus_cannon_labels.py
+++ train_cannon_model/plot_galah_versus_cannon_labels.py
@@ -5,7 +5,7 @@ from astropy.io import fits
 import matplotlib.pyplot as plt
 
 # load cannon model
-model = tc.CannonModel.read('./cannon_models/galah_labels_4para_highSNR_precise_labels.model')
+model = tc.CannonModel.read('./cannon_models/galah_labels_5para_highSNR_precise_labels.model')
 
 # load flux data, training + test sets, cannon model
 w = fits.open('./data_files/gaia_rvs_wavelength.fits')[0].data[20:-20]
@@ -21,7 +21,7 @@ feh_galah, feh_cannon = [], []
 alpha_galah, alpha_cannon = [], []
 vbroad_galah, vbroad_cannon = [], []
 
-df_to_plot = test_set
+df_to_plot = training_set
 for source_id in df_to_plot.source_id.to_numpy()[:3000]:
     row = df_to_plot.loc[df_to_plot.source_id==source_id]
     teff_galah.append(row['galah_teff'].to_numpy()[0])
@@ -34,46 +34,46 @@ for source_id in df_to_plot.source_id.to_numpy()[:3000]:
     ivar = 1/flux_err_data[str(source_id)].to_numpy()[20:-20]**2
     result = model.test(flux, ivar)
     
-    #teff_fit, logg_fit, feh_fit, alpha_fit, vbroad_fit = result[0][0]
-    teff_fit, logg_fit, feh_fit, vbroad_fit = result[0][0]
+    teff_fit, logg_fit, feh_fit, alpha_fit, vbroad_fit = result[0][0]
+    #teff_fit, logg_fit, feh_fit, vbroad_fit = result[0][0]
     
     teff_cannon.append(teff_fit)
     logg_cannon.append(logg_fit)
     feh_cannon.append(feh_fit)
-    #alpha_cannon.append(alpha_fit)
+    alpha_cannon.append(alpha_fit)
     vbroad_cannon.append(vbroad_fit)
 
 
 plt.figure(figsize=(20,5))
 alpha_value = 0.5
 plt.rcParams['font.size']=15
-plt.subplot(141)
+plt.subplot(151)
 plt.plot(teff_galah, teff_cannon, '.', color='dimgrey', ms=3, alpha=alpha_value)
 plt.xlabel('GALAH teff');plt.ylabel('Cannon teff')
 plt.plot([3000,9000], [3000,9000], lw=0.7, color='#AA8ED9')
 
 
-plt.subplot(142)
+plt.subplot(152)
 plt.plot(logg_galah, logg_cannon, '.', color='dimgrey', ms=3, alpha=alpha_value)
 plt.xlabel('GALAH logg');plt.ylabel('Cannon logg')
 plt.plot([3,5], [3,5], lw=0.7, color='#AA8ED9')
 
 
-plt.subplot(143)
+plt.subplot(153)
 plt.plot(feh_galah, feh_cannon, '.', color='dimgrey', ms=3, alpha=alpha_value)
 plt.xlabel('GALAH feh');plt.ylabel('Cannon feh')
 plt.plot([-2,2], [-2,2], lw=0.7, color='#AA8ED9')
 
-# plt.subplot(154)
-# plt.plot(alpha_galah, alpha_cannon, '.', color='dimgrey', ms=3, alpha=alpha_value)
-# plt.xlabel('GALAH alpha');plt.ylabel('Cannon alpha')
-# plt.plot([-2,2], [-2,2], lw=0.7, color='#AA8ED9')
+plt.subplot(154)
+plt.plot(alpha_galah, alpha_cannon, '.', color='dimgrey', ms=3, alpha=alpha_value)
+plt.xlabel('GALAH alpha');plt.ylabel('Cannon alpha')
+plt.plot([-2,2], [-2,2], lw=0.7, color='#AA8ED9')
 
-plt.subplot(144)
+plt.subplot(155)
 plt.loglog(vbroad_galah, vbroad_cannon, '.', color='dimgrey', ms=3, alpha=alpha_value)
 plt.xlabel('GALAH vbroad');plt.ylabel('Cannon vbroad')
 plt.plot([0.1,100], [0.1,100], lw=0.7, color='#AA8ED9')
 
 plt.subplots_adjust(wspace=0.3)
 plt.rcParams['figure.dpi']=300
-plt.savefig('/Users/isabelangelo/Desktop/cannon_galah_labels.png')
+plt.savefig('/Users/isabelangelo/Desktop/cannon_galah_labels_training.png')
