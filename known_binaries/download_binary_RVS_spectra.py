565f57139be43ae3c05532f1747e3979855a7786
Isabel Angelo <isabelangelo@astro.ucla.edu>
2023-04-26 17:27:05 -0400

    updated compute_galah_versus_cannon_labels to handle APOGEE data

--- known_binaries/download_binary_RVS_spectra.py
+++ known_binaries/download_binary_RVS_spectra.py
@@ -9,8 +9,14 @@ query = f"SELECT dr3.designation, galah.sobject_id, dr3.source_id \
 FROM user_iangelo.galah_binaries_filtered as galah \
 JOIN gaiadr3.gaia_source as dr3 \
     ON dr3.source_id = galah.source_id"
-    
-job = Gaia.launch_job_async(query)
+
+pw2020_query = f"SELECT dr3.designation, pw2020.apogee_id, dr3.source_id \
+FROM user_iangelo.pricewhelan2020_binaries_filtered as pw2020 \
+JOIN gaiadr3.gaia_source as dr3 \
+    ON dr3.source_id = pw2020.gaiadr3_source_id \
+WHERE dr3.rvs_spec_sig_to_noise > 50"
+
+job = Gaia.launch_job_async(pw2020_query)
 results = job.get_results()
 print(f'Table size (rows): {len(results)}')
 
@@ -52,8 +58,8 @@ for i in range(len(datalink[dl_key])):
 flux_dict = dict(zip(source_id_list, flux_list))
 flux_err_dict = dict(zip(source_id_list, flux_error_list))
 
-flux_filename = './data_files/flux_data_binaries.csv'
-flux_err_filename = './data_files/flux_err_data_binaries.csv'
+flux_filename = './data_files/flux_data_pw2020.csv' # flux_data_binaries
+flux_err_filename = './data_files/flux_err_data_pw2020.csv' # flux_err_data_binaries
 pd.DataFrame(flux_dict).to_csv(flux_filename, index=False)
 pd.DataFrame(flux_err_dict).to_csv(flux_err_filename, index=False)
 print('flux data saved to {}'.format(flux_filename))
