3013cf2806fca255f738b9131ad5aeb65fc08faf
Isabel Angelo <isabelangelo@astro.ucla.edu>
2023-08-08 14:36:17 -0700

    added script to download single + binary test data from El-Badry 2018

---
+++ load_elbadry2018_data.py
@@ -0,0 +1,97 @@
+import astropy.table as at
+import pandas as pd
+import gaia
+
+########### load data from El-Badry 2018 ################################################
+
+# Table E1: targets identified as single stars
+elbadry_stars = at.Table.read(
+    './data/literature_tables/ElBadry2018b_tables/Table_E1_all_single_star_ids.csv')
+
+# Table E3: targets identified as binaries in which both components contribute to the spectrum.
+elbadry_binaries = at.Table.read(
+    './data/literature_tables/ElBadry2018b_tables/Table_E3_all_binary_star_labels.csv')
+elbadry_binaries.rename_column('APOGEE_ID', 'apogee_id')
+
+# APOGEE DR13/Gaia DR3 crossmatch from Adrian
+gaia_apogee_xmatch = at.Table.read('../allStar-dr17-synspec-gaiadr3-gaiasourcelite.fits')
+gaia_apogee_xmatch.rename_column('APOGEE_ID', 'apogee_id')
+print('\n{} single stars, {} binaries listed in El-Badry 2018 Tables E1+E3\n'.format(
+	len(elbadry_binaries),
+	len(elbadry_stars)))
+
+########### crossmatch APOGEE IDs/gaia and filter sample ################################## 
+# require has_rvs = True, since this information is available
+
+# vet single stars with Gaia parameters
+elbadry_stars_gaia = at.join(elbadry_stars, gaia_apogee_xmatch, keys='apogee_id').to_pandas()
+print('{} single stars found in Gaia-APOGEE crossmatch'.format(len(elbadry_stars_gaia)))
+elbadry_stars_gaia = elbadry_stars_gaia.query("non_single_star == 0 & ruwe < 1.4 & has_rvs == True")
+print('{} remain with non_single_star=0, RUWE<1.4, has_rvs=True\n'.format(len(elbadry_stars_gaia)))
+# keep full binary sample
+elbadry_binaries_gaia = at.join(elbadry_binaries, gaia_apogee_xmatch, keys='apogee_id').to_pandas()
+print('{} binaries found in Gaia-APOGEE crossmatch'.format(len(elbadry_binaries_gaia)))
+elbadry_binaries_gaia = elbadry_binaries_gaia.query('has_rvs == True')
+print('{} remain with has_rvs=True\n'.format(len(elbadry_binaries_gaia)))
+
+# merge full sample, preserving binary/single star labels
+# store type for sorting
+elbadry_stars_gaia['type'] = 'single' 
+elbadry_binaries_gaia['type'] = 'binary' # store type for sorting
+
+# make sample size the same, only preserve common columns
+elbadry_stars_left = elbadry_stars_gaia.sample(n=len(elbadry_binaries_gaia), random_state=1234)
+elbadry_binaries_right = elbadry_binaries_gaia[elbadry_stars_gaia.columns]
+
+
+columns_to_keep = ['apogee_id','source_id','type']
+elbadry_full_sample_gaia = pd.concat(
+    (elbadry_stars_left, elbadry_binaries_right))[columns_to_keep]
+print('querying equal sample sizes of N={} for single stars, binaries'.format(
+	len(elbadry_binaries_gaia)))
+
+########### upload to gaia to download RVS spectra ##################################
+query = f"SELECT  eb2018.apogee_id, eb2018.source_id, dr3.designation, eb2018.type, \
+dr3.rvs_spec_sig_to_noise, dr3.ra, dr3.dec, \
+dr3.non_single_star, dr3.ruwe \
+FROM user_iangelo.elbadry_full_sample_gaia as eb2018 \
+JOIN gaiadr3.gaia_source as dr3 \
+ON dr3.source_id = eb2018.source_id \
+WHERE dr3.has_rvs = 'True' \
+AND dr3.rvs_spec_sig_to_noise > 50"
+
+# query gaia and download RVS spectra, save to dataframes
+# gaia.upload_table(elbadry_full_sample_gaia, 'elbadry_full_sample_gaia')
+elbadry_full_sample_gaia_results, flux_df, sigma_df = gaia.retrieve_data_and_labels(query)
+print('{} with has_rvs = True'.format(len(elbadry_full_sample_gaia_results)))
+print('saving flux, flux_err to .csv files')
+
+# write single + binary labels to .csv files
+elbadry_stars_gaia_results = elbadry_full_sample_gaia_results.query("type == 'single' ")
+elbadry_binaries_gaia_results = elbadry_full_sample_gaia_results.query("type == 'binary' ")
+gaia.write_labels_to_file(
+	elbadry_stars_gaia_results, 
+	'elbadry_singles')
+gaia.write_labels_to_file(
+	elbadry_binaries_gaia_results, 
+	'elbadry_tableE3_binaries')
+
+# write single + binary flux, sigma to .csv files
+single_source_ids = elbadry_stars_gaia_results.source_id.to_numpy()
+binary_source_ids = elbadry_binaries_gaia_results.source_id.to_numpy()
+print('{} are single stars, {} are binaries'.format(
+	len(single_source_ids),
+	len(binary_source_ids)))
+	
+gaia.write_flux_data_to_csv(
+	flux_df[single_source_ids], 
+	sigma_df[single_source_ids], 
+	'elbadry_singles')
+
+gaia.write_flux_data_to_csv(
+	flux_df[binary_source_ids],
+	 sigma_df[binary_source_ids], 
+	 'elbadry_tableE3_binaries')
+
+
+
