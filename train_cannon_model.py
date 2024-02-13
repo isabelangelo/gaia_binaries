232720132a12de35e48de808821eea825c59d82d
Isabel Angelo <isabelangelo@astro.ucla.edu>
2024-02-12 16:09:26 -0800

    added function to compute empirical s2 and store as model attribute:

--- train_cannon_model.py
+++ train_cannon_model.py
@@ -8,43 +8,40 @@ import thecannon as tc
 import gaia_spectrum
 import pandas as pd
 
+################# define variables and functions for training ###################################################
+
 # path to save model files to, 
 # should be descriptive of current model to be trained
 model_fileroot = 'gaia_rvs_model'
 
-################# define training set + get labels ###################################################
-# define training set labels
-training_labels = ['galah_teff', 'galah_logg','galah_feh', 'galah_alpha', 'galah_vbroad']
+def update_empirical_s2(model):
+    """
+    function to compute empirical model scatter and update model.s2_emp parameter
+    
+    Args:
+        model (tc.model.CannonModel) : Cannon model object to compute s2
+    Returns:
+        model (tc.model.CannonModel) : updated Cannon model object with s2_emp attribute
+    """
+    # compute |data - model| for high SNR targets
+    s2_emp_arr = []
+    for source_id in high_snr_flux_df.columns.to_numpy()[1:]:
+        # load flux, sigma of target
+        flux = high_snr_flux_df[source_id]
+        sigma = high_snr_sigma_df[source_id]
+        # mask calcium features for fit
+        spec = gaia_spectrum.GaiaSpectrum(int(source_id), flux, sigma)
+        # store per pixel |data-model|
+        s2_emp_arr.append(abs(flux - spec.single_fit))
+    # compute per pixel empirical s2, save as model attribute
+    s2_emp_arr = np.array(s2_emp_arr).T
+    model.s2_emp = np.mean(s2_emp_arr, axis=1)**2
 
-# Load the table containing the training set labels
-training_set_table = Table.read('./data/label_dataframes/training_labels.csv', format='csv')
-training_set = training_set_table[training_labels]
-
-# normalized_sigma = np.nan_to_num(normalized_sigma_nan, nan=1)
-normalized_flux = fits.open('./data/cannon_training_data/training_flux.fits')[0].data
-normalized_sigma = fits.open('./data/cannon_training_data/training_sigma.fits')[0].data
-normalized_ivar = 1/normalized_sigma**2
-
-
-# Create a vectorizer that defines our model form.
-vectorizer = tc.vectorizer.PolynomialVectorizer(training_labels, 2)
-
-# Create the model that will run in parallel using all available cores.
-model_0 = tc.CannonModel(training_set, normalized_flux, normalized_ivar,
-                       vectorizer=vectorizer, regularization=None)
-
-# train initial model and write to file for reference
-model_0.train()
-import pdb;pdb.set_trace()
-print('finished training first iteration of cannon model')
-model_filename_initial = './data/cannon_models/' + model_fileroot + '_initial.model'
-model_0.write(model_filename_initial, include_training_set_spectra=True)
-
-# iteratively clean and re-train model
 def clean(model_iter_n):
     """
     function to clean (i.e. remove binaries) from training set and re-train
-    binaries are identified as objects with delta_chisq>100
+    binaries are identified as objects with delta_chisq>100, 
+    used for iterative cleaning
     
     Args:
         model_iter_n (tc.model.CannonModel) : Cannon model object to clean
@@ -83,6 +80,32 @@ def clean(model_iter_n):
     model_iter_n_plus_1.train()
     return model_iter_n_plus_1
 
+################# train Cannon model ###################################################
+# define training set labels
+training_labels = ['galah_teff', 'galah_logg','galah_feh', 'galah_alpha', 'galah_vbroad']
+
+# Load the table containing the training set labels
+training_set_table = Table.read('./data/label_dataframes/training_labels.csv', format='csv')
+training_set = training_set_table[training_labels]
+
+# normalized_sigma = np.nan_to_num(normalized_sigma_nan, nan=1)
+normalized_flux = fits.open('./data/cannon_training_data/training_flux.fits')[0].data
+normalized_sigma = fits.open('./data/cannon_training_data/training_sigma.fits')[0].data
+normalized_ivar = 1/normalized_sigma**2
+
+# Create a vectorizer that defines our model form.
+vectorizer = tc.vectorizer.PolynomialVectorizer(training_labels, 2)
+
+# Create the model that will run in parallel using all available cores.
+model_0 = tc.CannonModel(training_set, normalized_flux, normalized_ivar,
+                       vectorizer=vectorizer, regularization=None)
+
+# train initial model and write to file for reference
+model_0.train()
+print('finished training first iteration of cannon model')
+model_filename_initial = './data/cannon_models/' + model_fileroot + '_initial.model'
+model_0.write(model_filename_initial, include_training_set_spectra=True)
+
 # clean until the model finds zero binaries in its training set
 model_n = model_0
 model_n_plus_1 = clean(model_0)
@@ -109,14 +132,16 @@ model_filename_cleaned = './data/cannon_models/' + model_fileroot + '_cleaned.mo
 model_cleaned.write(model_filename_cleaned, include_training_set_spectra=True)
 print('cleaned model written to {}'.format(model_filename_cleaned))
 
-# print training set GALAH label errors
-print('average GALAH label uncertainties in training set:')
-for i in range(len(training_labels)):
-	label = training_labels[i]
-	label_uncertainty = model_cleaned.training_set_labels.T[i].mean()
-	print('{}: {}'.format(
-		label,
-		np.mean(training_set_table[label.replace('_', '_e')])))
+# # print training set GALAH label errors
+# print('average GALAH label uncertainties in training set:')
+# for i in range(len(training_labels)):
+# 	label = training_labels[i]
+# 	label_uncertainty = model_cleaned.training_set_labels.T[i].mean()
+# 	print('{}: {}'.format(
+# 		label,
+# 		np.mean(training_set_table[label.replace('_', '_e')])))
+
+################# update training data and save to files ###################################################
 
 # save the full training set labels + flux for the cleaned model
 training_set_table_df = training_set_table.to_pandas() # need pandas version to query
@@ -139,3 +164,15 @@ training_sigma_df_cleaned = training_sigma_df[[str(i) for i in training_labels_c
 training_labels_cleaned.to_csv('./data/label_dataframes/training_labels_cleaned.csv')
 training_flux_df_cleaned.to_csv('./data/gaia_rvs_dataframes/training_flux_cleaned.csv')
 training_sigma_df_cleaned.to_csv('./data/gaia_rvs_dataframes/training_sigma_cleaned.csv')
+
+
+# okay so I added an update_s2
+# but now I need to make sure the s2 gets updated before being written to a file.
+# and before it's used for iterative cleaning.
+# should I include it as a step in the iterative cleaning? maybe the first step?
+# let me take a second to think about this
+# maybe I need to work on this tomorrow?
+
+# and then I need to make sure the cleaning process is using the s2_emp.
+# then I can print the stuff and go home
+# and maybe grade exams tomorrow.
